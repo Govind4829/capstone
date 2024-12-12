282436cc4c898b84de48b69b8675f7284cdd385c
root <root@ip-172-31-19-178.ec2.internal>
2024-12-12 07:23:49 +0000

    Initial commit with .gitignore and .dockerignore files

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+# Set variables
+IMAGE_NAME="apache4-app"  # Name of the Docker image
+TAG="latest"  # Tag for the image (e.g., latest, dev, prod)
+
+# Step 1: Ensure we are in the directory with the Dockerfile
+SCRIPT_DIR=$(/home/ubuntu/apache2-app "$0")
+cd "$SCRIPT_DIR" || exit 1
+
+# Step 2: Build the Docker image
+echo "Building Docker image $apache4-app:$latest"
+docker build -t "$IMAGE_NAME:$TAG" .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image $IMAGE_NAME:$TAG built successfully."
+else
+    echo "Error: Docker image build failed."
+    exit 1
+fi
+
+# Optional: Push to Docker Hub (Uncomment if you want to push it)
+# echo "Pushing Docker image to Docker Hub..."
+# docker push "$IMAGE_NAME:$TAG"
---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set variables
+IMAGE_NAME="praticeuser/dev"  # Replace with your Docker image name
+TAG="latest"  # Replace with the image tag (e.g., 'prod', 'latest', etc.)
+CONTAINER_NAME="httpd"  # The name of the running Docker container
+PORT="80"  # Port to expose from the container
+NEW_PORT="8050"  # New port to expose the container
+
+# Step 1: Ensure Docker is installed
+
+# Step 2: Pull the latest image from Docker registry
+echo "Pulling the latest Docker image $IMAGE_NAME:$TAG..."
+docker pull $IMAGE_NAME:$TAG
+
+# Step 3: Stop and remove any running container with the same name
+#echo "Stopping and removing any existing container named $CONTAINER_NAME..."
+#docker stop $CONTAINER_NAME
+#docker rm $CONTAINER_NAME
+
+# Step 4: Run the new container with the pulled image
+echo "Running the container with the image $IMAGE_NAME:$TAG..."
+docker run -d --name $CONTAINER_NAME -p $NEW_PORT:$PORT $IMAGE_NAME:$TAG
+
+# Step 5: Verify if the container is running
+if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
+    echo "Container $CONTAINER_NAME is now running with the image $IMAGE_NAME:$TAG."
+else
+    echo "Error: Failed to start the container."
+    exit 1
+fi
+
